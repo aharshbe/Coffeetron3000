f3bfc22b93ee7bf12999745b30df34ab38428738
Austin Harshberger <aharshberger@sf-m-aharshberger.ideo.com>
2017-05-11 17:11:47 -0700

    adding

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
 <?php header( 'Location: Coffee-Tron/index.html' ) ;  ?>
+<link rel="stylesheet" type="text/css" href="../styles/styles.css">
+
+<style type="text/css">
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
+    <script src="../jquery.easing.1.3.js" type="text/javascript" charset="utf-8"></script>
+    <script src="../jquery.jSlots.min.js" type="text/javascript" charset="utf-8"></script>
+    <script type="text/javascript" charset="utf-8">
